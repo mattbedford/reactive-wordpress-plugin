3e16d284cbeb808d915764dca7b45bf17c3d78e8
Matt-at-CS <matt.bedford@classroomsecrets.co.uk>
2025-01-08 16:49:38 +0100

    First commit

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: reactive-wordpress
+ * Description: Adds global theme support for using React JS in a WordPressy way.
+ * Version:     1.2.0
+ * Author:      Dev Team
+ * Text Domain: reactivewordpress
+ * Requires PHP:    8.2
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
